3d4d237d348c5e7daee06b21c10abab532928e94
OLT2000 <olliethurston1@gmail.com>
2024-08-05 13:18:39 +0100

    Added migration for new columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_05_103814) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_05_120331) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_05_103814) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "mood", limit: 2
+    t.integer "sleep", limit: 2
+    t.integer "steps", default: 0
+    t.integer "protein", default: 0
+    t.integer "carbs", default: 0
+    t.integer "fats", default: 0
+    t.virtual "calories_in", type: :integer, as: "(((4 * protein) + (4 * carbs)) + (9 * fats))", stored: true
   end
 
 end
