e7887969a9514e64a9604c242630f1cfe5d26c06
OLT2000 <olliethurston1@gmail.com>
2024-08-06 09:51:57 +0100

    working date scroller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,12 +1,24 @@
 class EntriesController < ApplicationController
+    before_action :set_current_date
+
     def index
         if user_signed_in?
-            @entries = Entry.where(user_id: current_user.id)
+            puts "Index Date #{@current_date}"
+            @entries = Entry.where(user_id: current_user.id, entry_date: @current_date)
         else
             redirect_to user_session_path, alert: "You must be signed in to do that!"
         end
     end
- 
+
+    def shift_date_left
+        @current_date = @current_date - 1.day
+        redirect_to root_path(date: @current_date)
+    end
+
+    def shift_date_right
+        @current_date = @current_date + 1.day
+        redirect_to root_path(date: @current_date)
+    end
  
     def new
         if user_signed_in?
@@ -30,10 +42,14 @@ class EntriesController < ApplicationController
 
     def destroy
         Entry.find(params[:id]).destroy
-        redirect_to root_url
+        render root_url
     end
  
     private
+
+    def set_current_date
+        @current_date = params[:date].present? ? Date.parse(params[:date]) : Date.today
+    end
  
     def entry_params
         params.require(:entry).permit(:title, :description, :mood, :sleep_hrs, :steps, :protein, :carbs, :fats)
