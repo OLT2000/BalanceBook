0681980339fa91604137614b9c428b2af6ee8dcf
OLT2000 <olliethurston1@gmail.com>
2024-08-06 16:52:49 +0100

    added more password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,68 @@ class User < ApplicationRecord
     # Include default devise modules. Others available are:
     # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
     devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable#, :validatable
 
-    
+    validate :password_contains_number
+    validate :password_contains_lowercase
+    validate :password_contains_uppercase
+    validate :password_contains_special_character
+    validate :password_length
+
+    validate :first_name_present
+    validate :last_name_present
+
+    private
+
+    def password_length
+        if password.length < 8
+          errors.add :password, " must be at least 8 characters long."
+          false
+        else
+          true
+        end
+    end
+
+    def password_contains_number
+      return if password.blank? || password.match?(/\d/)
+  
+      errors.add(:password, 'must contain at least one number.')
+    end
+
+    def password_contains_uppercase
+      return if password.blank? || password.match?(/[A-Z]/)
+  
+      errors.add(:password, 'must contain at least one uppercase letter.')
+    end
+
+    def password_contains_lowercase
+      return if password.blank? || password.match?(/[a-z]/)
+  
+      errors.add(:password, 'must contain at least one lowercase letter.')
+    end
+
+    def password_contains_special_character
+      return if password.blank? || password.match?(/[\W_]/)
+  
+      errors.add(:password, 'must contain at least one special character.')
+    end
+
+    def first_name_present
+      if first_name.blank?
+        errors.add :first_name, "cannot be blank."
+        false
+      else
+        true
+      end
+    end
+
+    def last_name_present
+      if last_name.blank?
+        errors.add :last_name, "cannot be blank."
+        false
+      else
+        true
+      end
+    end
+
 end
