4430c1e45f776e915f2ec0ccd345d25652dd77ff
OLT2000 <olliethurston1@gmail.com>
2024-08-06 08:53:57 +0100

    Added date paginator header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :set_today_date, if: :user_signed_in?
+
+    private
+
+    def set_today_date
+        @current_date = Date.today
+    end
 
     protected
 
