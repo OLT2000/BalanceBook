32e1b11fcb6bcc1ee6442b9096a6470f51225b28
OLT2000 <olliethurston1@gmail.com>
2024-08-06 13:17:48 +0100

    modified accessibility tags

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,14 +1,19 @@
 module EntriesHelper
     def entry_form(entry)
         form_with(model: entry) do |form|
-            concat content_tag(:h3, "How are you feeling?", class: "text-lg font-semibold mb-2", style: "text-align: center")
+            concat content_tag(:h1, "Log Your Day", class: "text-lg font-semibold mb-2", style: "text-align: center")
+            concat form.label(:title, "Enter your title...")
             concat form.text_field(:title, class: "form-control border-primary mb-1", required: true, placeholder: "Enter title...")
+
+            concat content_tag(:h2, "How are you feeling?", class: "text-lg font-semibold mb-2", style: "text-align: center")
+
+            concat form.label(:description, "Enter your description...")
             concat form.text_field(:description, class: "form-control border-primary mb-1", required: true, placeholder: "Enter description...")
         
             concat form.label(:mood, "Rate your mood between 0 and 10")
             concat form.select(:mood, 0..10, class: "form-control border-primary mb-1")
         
-            concat content_tag(:h3, "Health Tracker", class: "text-lg font-semibold mb-2", style: "text-align: center")
+            concat content_tag(:h2, "Health Tracker", class: "text-lg font-semibold mb-2", style: "text-align: center")
         
             concat form.label(:sleep_hrs, "Enter your sleep in hours...")
             concat form.number_field(:sleep_hrs, max: 24, class: "form-control border-primary mb-1")
