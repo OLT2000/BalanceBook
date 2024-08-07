b9dcafefd9726d8ee3db9034d95fb20e668e5b9e
OLT2000 <olliethurston1@gmail.com>
2024-08-07 13:37:13 +0100

    Added new form validations

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,16 @@
 class Entry < ApplicationRecord
     belongs_to :user, foreign_key: "user_id"
+
+    numerical_inputs = [
+        :mood, :steps, :sleep_hrs, :protein, :carbs, :fats
+    ]
+    
+    numerical_inputs.each do |key|
+        validates key, numericality: { greater_than_or_equal_to: 0 }
+    end
+
+    validates :steps, numericality: { only_integer: true }
+
+    validates :sleep_hrs, numericality: { less_than_or_equal_to: 24 }
+    
 end
